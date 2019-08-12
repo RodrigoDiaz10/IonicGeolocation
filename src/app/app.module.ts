c39da3f817fc7e11f5d7e6b973e497ef16b6729d
RodrigoDiaz10 <raa.diaz@yavirac.edu.ec>
2019-08-12 01:12:20 -0500

    ionic y sqlite geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ModalPageModule } from './modal/modal.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -9,14 +10,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+import { GoogleMaps } from '@ionic-native/google-maps';
+
+import { Camera } from '@ionic-native/camera/ngx';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+import { DbService } from '../app/db.service';
+
+
+
+
+
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ModalPageModule],
   providers: [
+    Camera,
+    SQLite,
+    DbService,
+    GoogleMaps,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Geolocation,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {  ModalController } from '@ionic/angular';
+import { DbService } from '../../app/db.service';
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,40 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  coords : any = { lat: 0, lng: 0 }
+  sitios: any;
+
+
+
+  constructor( 
+    //public navParams: NavParams,
+    private modalController: ModalController,
+    public db : DbService
+    ) {}
+
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad Tab3Page');
+    }
+  async closeModal() {
+    const onClosedData: string = "Wrapped Up!";
+    await this.modalController.dismiss(onClosedData);
+  }
+
+  ionViewDidEnter(){
+    this.db.getSitios().then((res)=>{
+   this.sitios = [];
+   for(var i = 0; i < res.rows.length; i++){
+       this.sitios.push({
+         id: res.rows.item(i).id,
+         lat: res.rows.item(i).lat,
+         lng: res.rows.item(i).lng,
+         address: res.rows.item(i).address,
+         description: res.rows.item(i).description,
+         foto: res.rows.item(i).foto
+       });
+   }
+
+   },(err)=>{ /* alert('error al sacar de la bd'+err) */ })
+  }
 
 }
